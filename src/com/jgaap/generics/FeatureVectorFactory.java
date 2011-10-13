eee1b596e2b7f07d959393a1099297905baca862
John Noecker Jr. <jnoecker@gmail.com>
2011-10-13 15:35:50 -0400

    Added support for normalized and relative frequency vector matrices to FeatureVectorFactory

--- src/com/jgaap/generics/FeatureVectorFactory.java
+++ src/com/jgaap/generics/FeatureVectorFactory.java
@@ -13,6 +13,12 @@ import java.util.Set;
  *
  */
 public class FeatureVectorFactory {
+	/**
+	 * Generate the absolute frequency vector matrices
+	 * @param known The known event sets
+	 * @param unknown The unknown event sets 
+	 * @return Pair of feature vector matrices
+	 */
 	public static Pair<Double[][], Double[][]> getFeatures(List<EventSet> known, List<EventSet> unknown) {
 		Set<Event> vocab = new HashSet<Event>();
 		
@@ -48,6 +54,102 @@ public class FeatureVectorFactory {
 			int j = 0;
 			for(Event event : vocab) {
 				unknownSet[i][j] = (double)histogram.getAbsoluteFrequency(event);
+				j++;
+			}
+		}
+		
+		return new Pair<Double[][], Double[][]>(knownSet, unknownSet);
+	}
+	
+	/**
+	 * Generate the relative frequency vector matrices
+	 * @param known The known event sets
+	 * @param unknown The unknown event sets 
+	 * @return Pair of feature vector matrices
+	 */
+	public static Pair<Double[][], Double[][]> getRelativeFeatures(List<EventSet> known, List<EventSet> unknown) {
+		Set<Event> vocab = new HashSet<Event>();
+		
+		// Add all events from known documents to the vocab list.
+		for(EventSet eventSet : known) {
+			for(Event event : eventSet) {
+				vocab.add(event);
+			}
+		}
+		
+		// Add all events from unknown documents to the vocab list.
+		for(EventSet eventSet: unknown) {
+			for(Event event : eventSet) {
+				vocab.add(event);
+			}
+		}
+		
+		// Construct the known vector
+		Double[][] knownSet = new Double[known.size()][vocab.size()];
+		for(int i = 0; i < known.size(); i++) {
+			EventHistogram histogram = new EventHistogram(known.get(i));
+			int j = 0;
+			for(Event event : vocab) {
+				knownSet[i][j] = (double)histogram.getRelativeFrequency(event);
+				j++;
+			}
+		}
+		
+		// Construct the unknown vector
+		Double[][] unknownSet = new Double[unknown.size()][vocab.size()];
+		for(int i = 0; i < unknown.size(); i++) {
+			EventHistogram histogram = new EventHistogram(unknown.get(i));
+			int j = 0;
+			for(Event event : vocab) {
+				unknownSet[i][j] = (double)histogram.getRelativeFrequency(event);
+				j++;
+			}
+		}
+		
+		return new Pair<Double[][], Double[][]>(knownSet, unknownSet);
+	}
+	
+	/**
+	 * Generate the normalized frequency vector matrices
+	 * @param known The known event sets
+	 * @param unknown The unknown event sets 
+	 * @return Pair of feature vector matrices
+	 */
+	public static Pair<Double[][], Double[][]> getNormalizedFeatures(List<EventSet> known, List<EventSet> unknown) {
+		Set<Event> vocab = new HashSet<Event>();
+		
+		// Add all events from known documents to the vocab list.
+		for(EventSet eventSet : known) {
+			for(Event event : eventSet) {
+				vocab.add(event);
+			}
+		}
+		
+		// Add all events from unknown documents to the vocab list.
+		for(EventSet eventSet: unknown) {
+			for(Event event : eventSet) {
+				vocab.add(event);
+			}
+		}
+		
+		// Construct the known vector
+		Double[][] knownSet = new Double[known.size()][vocab.size()];
+		for(int i = 0; i < known.size(); i++) {
+			EventHistogram histogram = new EventHistogram(known.get(i));
+			int j = 0;
+			for(Event event : vocab) {
+				knownSet[i][j] = (double)histogram.getNormalizedFrequency(event);
+				j++;
+			}
+		}
+		
+		// Construct the unknown vector
+		Double[][] unknownSet = new Double[unknown.size()][vocab.size()];
+		for(int i = 0; i < unknown.size(); i++) {
+			EventHistogram histogram = new EventHistogram(unknown.get(i));
+			int j = 0;
+			for(Event event : vocab) {
+				unknownSet[i][j] = (double)histogram.getNormalizedFrequency(event);
 				j++;
 			}
 		}
